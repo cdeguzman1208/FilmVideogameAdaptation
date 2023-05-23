591b5eef91876a16e75f18008fcc90fdbfd1ab9c
Cromwell <cdeguzman1208@gmail.com>
2023-05-23 15:32:58 -0700

    add loading bar

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,7 +4,20 @@ class Load extends Phaser.Scene {
     }
 
     preload() {
-        
+        // loading bar
+        // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/loader/
+        let loadingBar = this.add.graphics()
+        this.load.on('progress', (value) => {
+            loadingBar.clear()
+            loadingBar.fillStyle(0xFFFFFF, 1)
+            loadingBar.fillRect(0, centerY, w * value, 5)
+        })
+        this.load.on('complete', () => {
+            loadingBar.destroy()
+        })
+
+        // set load path
+        this.load.path = './assets/art'
     }
 
     create() {
