6de290d291d73df9fae9c2bc4c94f8349ce9571b
Beatrice Yu <besyu@ucsc.edu>
2023-05-25 17:42:53 -0700

    border

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,5 +1,16 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, velocity) {
         super(scene, x, y, 'player');
+
+        // maintain scene context 
+        this.parentScene = scene
+
+        // set up physics sprite 
+        this.parentScene.add.existing(this); 
+        this.parentScene.physics.add.existing(this);
+    }
+
+    update() {
+        
     }
 }
