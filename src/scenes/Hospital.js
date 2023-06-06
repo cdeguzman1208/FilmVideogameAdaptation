17ed1c84c6495c5066a4b381a693010cad9ee3ed
Beatrice Yu <besyu@ucsc.edu>
2023-06-05 20:56:40 -0700

    international klein blue

--- src/scenes/Hospital.js
+++ src/scenes/Hospital.js
@@ -9,7 +9,7 @@ class Hospital extends Phaser.Scene {
         // input
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.blue = this.add.rectangle(0, 0, 480, 320, 0x0000ff).setOrigin(0); 
+        this.blue = this.add.rectangle(0, 0, 480, 320, 0x002199).setOrigin(0); 
         this.t1 = this.add.text(240, 100, 'The road to the city of Aqua Vitae is protected by a labyrinth').setOrigin(0.5); 
         this.t2 = this.add.text(240, 150, 'Built from crystals and mirrors, which in the sunlight,').setOrigin(0.5); 
         this.t3 = this.add.text(240, 200, 'Causes terrible blindness...').setOrigin(0.5); 
